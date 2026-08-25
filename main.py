33f0f47581420d6a9ad1bbc8dacd51af5b441091
Szabson <szymonzaba18@gmail.com>
2026-08-25 18:04:12 +0200

    hours

--- main.py
+++ main.py
@@ -180,9 +180,9 @@ def get_machine_status(machine_name, conn: psycopg.Connection = Depends(get_db))
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail={
-                    "error": f"Machine validation is invalid or expired {hours} passed", 
+                    "error": f"Machine validation is invalid or expired 8 hours passed", 
                     "code": "Machine Invalidate", 
-                    "valid_hours": f"{hours}"
+                    "valid_hours": "8"
                 }
             )
         
